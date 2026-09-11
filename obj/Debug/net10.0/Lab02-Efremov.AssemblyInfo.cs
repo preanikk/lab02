c8851823a25d25edeff96e691bd865ed7124bc69
preanikk <preanik1x@gmail.com>
2026-09-11 11:06:53 +0300

    Lab02: var keyword

--- obj/Debug/net10.0/Lab02-Efremov.AssemblyInfo.cs
+++ obj/Debug/net10.0/Lab02-Efremov.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Lab02-Efremov")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+5e8b7e5e8e8e73e41ac4289cb1e07c3fc046c4b1")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2761466c425956c1cd6f4425e8732ef558303297")]
 [assembly: System.Reflection.AssemblyProductAttribute("Lab02-Efremov")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Lab02-Efremov")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
